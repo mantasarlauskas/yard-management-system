8a94c1d6a434654a720aae4d3c082cc9e6f4872a
ausra <ausra.cepulionyte@ktu.edu>
2019-05-11 18:32:16 +0300

    vs2017 projektas

---
+++ yard-management-system/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace yard_management_system.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
